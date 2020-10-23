fefb953d757c92ff0c396135494e56ba94e20b08
om rave <crave627@gmail.com>
2020-10-23 17:01:29 +0200

    Print sorted symptoms to file

--- src/main/java/com/hemebiotech/analytics/SymptomStatistics.java
+++ src/main/java/com/hemebiotech/analytics/SymptomStatistics.java
@@ -2,6 +2,7 @@ package com.hemebiotech.analytics;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /**
@@ -15,7 +16,7 @@ public class SymptomStatistics implements ISymptomOperation {
     @Override
     public Map<String, Long> symptomCounter(List<String> symptoms) {
         return symptoms.stream()
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+                .collect(Collectors.groupingBy(e -> e, TreeMap::new, Collectors.counting()));
     }
 
     /**
