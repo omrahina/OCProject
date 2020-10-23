c32a860c64ab10aa02a356b1947a35700d0455c4
om rave <crave627@gmail.com>
2020-10-23 12:12:47 +0200

    Added symptomCounter test

---
+++ src/test/java/com/hemebiotech/analytics/SymptomStatisticsTest.java
@@ -0,0 +1,25 @@
+package com.hemebiotech.analytics;
+
+import com.hemebiotech.analytics.SymptomStatistics;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SymptomStatisticsTest {
+
+    @Test
+    public void symptomCounter_ok(){
+        List<String> symptoms = Arrays.asList("headache", "insomnia", "nausea", "insomnia");
+        SymptomStatistics symptomStatistics = new SymptomStatistics();
+        Map<String, Long> counts = symptomStatistics.symptomCounter(symptoms);
+        assertTrue(counts.size() == 3);
+        assertEquals(2, counts.get("insomnia"));
+    }
+}
